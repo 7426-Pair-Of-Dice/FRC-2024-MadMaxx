01032150cc96100adb57b431e2877c8645d49585
Ohlin Arellano <ohlin.arellano@gmail.com>
2024-04-14 00:24:07 -0700

    updated readme

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.RobotContainer.RobotState;
@@ -96,10 +95,7 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {
-
-    
-  }
+  public void teleopPeriodic() {}
 
   @Override
   public void teleopExit() {}
